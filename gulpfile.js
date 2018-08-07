90f6bf82dcd8411d8ab7b6e80aca16337ae4ed95
Lo Whai Jer <neknek25251989@gmail.com>
2018-08-07 16:50:33 +0800

    Add local host Fix some bugs

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,28 @@
 // Less configuration
 var gulp = require('gulp');
 var less = require('gulp-less');
-var  gulpUglify = require('gulp-uglify');  // 載入 gulp-uglify
+var gulpUglify = require('gulp-uglify');  // 載入 gulp-uglify
 var cleanCSS = require('gulp-clean-css');
+var connect = require('gulp-connect');
+
+gulp.task('webserver', function() {
+  connect.server({
+    livereload: true
+  });
+});
 
 gulp.task('watch', function () {
-  gulp.watch('javascript/original/*.js', ['scripts']);
+  gulp.watch('javascript/original/*.js', ['script']);
   gulp.watch('css/less/*.less',['less']);
+  gulp.watch('css/*.css',['minify-css']);
+  
 });
 
 gulp.task('script', function () {
   gulp.src('javascript/original/*.js')        // 指定要處理的原始 JavaScript 檔案目錄
       .pipe(gulpUglify())                     // 將 JavaScript 做最小化
       .pipe(gulp.dest('javascript/minify'));  // 指定最小化後的 JavaScript 檔案目錄
+      
 });
 
 gulp.task('less', function() {
@@ -27,6 +37,6 @@ gulp.task('minify-css', () => {
     .pipe(gulp.dest('css/minifycss'));
 });
 
-gulp.task('default', ['less','script','minify-css','watch'], function() {
+gulp.task('default', ['less','script','minify-css','watch','webserver'], function() {
     gulp.watch('*.less', ['less']);
 })
