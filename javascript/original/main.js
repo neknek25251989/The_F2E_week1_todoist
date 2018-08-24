fe3e8dd77fe4ad0542f0f82a916fa461eb9d769d
Lo Azer <neknek25251989@gmail.com>
2018-08-24 17:18:50 +0800

    [mod] 補上完整功能，可讀取localstorage

--- javascript/original/main.js
+++ javascript/original/main.js
@@ -1,5 +1,21 @@
 window.onload = function () {
+	var oTitle = document.getElementsByClassName('header')[0];
+	var aNav = oTitle.getElementsByTagName('a');
+	var taskInProgress = document.getElementsByClassName('in-progress');
+	var taskCompleted = document.getElementsByClassName('task-completed');
+	var oEditTaskDetail = document.getElementsByClassName('new-note-detail')[0];
+
+	var oaddNewNote = document.getElementsByClassName('add-new-note')[0];
+  var oBtAdd = document.getElementsByClassName('add-task')[0];
+  var oNewTitle = oaddNewNote.getElementsByTagName('input')[0];
 	
+	var oBtCancel = document.getElementsByClassName('cancel')[0];
+
+	var oTaskListComp = document.getElementsByClassName('task-list-completed')[0];
+  var oTaskList = document.getElementsByClassName('task-list')[0];
+  var oTaskCount = document.getElementsByClassName('task');
+  var oTotalTask = document.getElementsByClassName('total-task')[0];
+	var oDeadLine = document.getElementsByClassName('time-input')[0];
 	// check local storage.
 	// if it is empty，make a empty array in it.
 	// else, asing a array that equal to array in local storage.
@@ -8,16 +24,63 @@ window.onload = function () {
 		var storageArray = [];
 	} else {		
 		var storageArray = JSON.parse(localStorage.getItem('item'));
-		console.log(storageArray)			
+		console.log(storageArray)	
+		var jsonArray = JSON.parse(localStorage.getItem('item'));
+		console.log(jsonArray);
+		
+		jsonArray.forEach(function (item) {
+
+			var newTask = document.createElement('div')
+
+			// hadle bar template
+			var source   = document.getElementById("entry-template").innerHTML;
+			var template = Handlebars.compile(source);
+
+			var context = {
+				content:item.content,
+				deadline:item.deadline,
+			};
+
+			var oTask = template(context);
+			newTask.className = 'task';
+			newTask.innerHTML = oTask;
+
+			oTaskList.insertBefore(newTask, oTaskList.childNodes[0]);
+
+		// delete this task
+
+		var oXIcon = document.getElementsByClassName('x-icon')[0];	
+		var oTaskText = document.getElementsByClassName('task-text')[0];
+
+		oXIcon.onclick = function () {
+
+			var jsonArray = JSON.parse(localStorage.getItem('item'));
+			console.log(jsonArray);
+
+			var deleteItem = jsonArray.find(function(obj){
+				return obj.content == oTaskText.innerHTML ;				
+			})
+			console.log(deleteItem);
+			deleteItem.isDelete = true;	
+
+			oXIcon.parentNode.parentNode.parentNode.removeChild(newTask);				
+			
+			storageArray = jsonArray.filter(function(e){
+				return e.isDelete !== true;
+			});
+			
+			localStorage.setItem('item', JSON.stringify(storageArray));
+				
+			oTotalTask.innerHTML = oTaskCount.length + ' ' + 'task left';
+		
+		}
+			
+	});				
 	}
 	
 	localStorage.clear();
 
-	var oTitle = document.getElementsByClassName('header')[0];
-	var aNav = oTitle.getElementsByTagName('a');
-	var taskInProgress = document.getElementsByClassName('in-progress');
-	var taskCompleted = document.getElementsByClassName('task-completed');
-	var oEditTaskDetail = document.getElementsByClassName('new-note-detail')[0];
+	
 
 	// default
   aNav[0].className = 'active';
@@ -92,11 +155,7 @@ window.onload = function () {
 	}
 
 	// onfocus & onblur
-	var oaddNewNote = document.getElementsByClassName('add-new-note')[0];
-  var oBtAdd = document.getElementsByClassName('add-task')[0];
-  var oNewTitle = oaddNewNote.getElementsByTagName('input')[0];
-	
-	var oBtCancel = document.getElementsByClassName('cancel')[0];
+
 	
 	
 	oNewTitle.onfocus = function() {
@@ -110,11 +169,7 @@ window.onload = function () {
 
 
   // add a new task
-	var oTaskListComp = document.getElementsByClassName('task-list-completed')[0];
-  var oTaskList = document.getElementsByClassName('task-list')[0];
-  var oTaskCount = document.getElementsByClassName('task');
-  var oTotalTask = document.getElementsByClassName('total-task')[0];
-	var oDeadLine = document.getElementsByClassName('time-input')[0];
+
 	
 
   oBtAdd.onclick = function () {
@@ -131,13 +186,17 @@ window.onload = function () {
 			deadline:oDeadLine.value,
 		};
 
-		inputObject = {content: context.content, deadline: context.deadline, isCheck: false, isDelete: false};
+		inputObject = {content: context.content, deadline: context.deadline, isDelete: false};
 
 		storageArray.push(inputObject);
 		stringJson = JSON.stringify(storageArray);
 		localStorage.setItem('item', stringJson);
 		
 		makeNewTask()		
+
+		oTotalTask.innerHTML = oTaskCount.length + ' ' + 'task left';
+
+	}
 
 		function makeNewTask() {		
 
@@ -161,56 +220,43 @@ window.onload = function () {
 				newTask.className = 'task';
 				newTask.innerHTML = oTask;
 
-				if (item.isCheck == true) {
-					oTaskListComp.appendChild(newTask);
-				} else {
-					oTaskList.insertBefore(newTask, oTaskList.childNodes[0]);	
-				}
+				oTaskList.insertBefore(newTask, oTaskList.childNodes[0]);
+
+			// delete this task
+	
+			var oXIcon = document.getElementsByClassName('x-icon')[0];	
+			var oTaskText = document.getElementsByClassName('task-text')[0];
+
+			oXIcon.onclick = function () {
+
+				var jsonArray = JSON.parse(localStorage.getItem('item'));
+				console.log(jsonArray);
+
+				var deleteItem = jsonArray.find(function(obj){
+					return obj.content == oTaskText.innerHTML ;				
+				})
+				console.log(deleteItem);
+				deleteItem.isDelete = true;	
+	
+				oXIcon.parentNode.parentNode.parentNode.removeChild(newTask);				
 				
-			});		
+				storageArray = jsonArray.filter(function(e){
+					return e.isDelete !== true;
+				});
+				
+				localStorage.setItem('item', JSON.stringify(storageArray));
+					
+				oTotalTask.innerHTML = oTaskCount.length + ' ' + 'task left';
+			
+			}
+				
+		});		
 			oDeadLine.value = null;
 			oNewTitle.value = null;
-		}
-
-
-
-
-		var oTaskText = document.getElementsByClassName('task-text')[0];
-
 	
-		// delete this task
-
-		var oXIcon = document.getElementsByClassName('x-icon')[0];
-
-
-		oXIcon.onclick = function () {
-			
-			clickoXIcon();
-
-			oTotalTask.innerHTML = oTaskCount.length + ' ' + 'task left';
-		
 		}
 
-		function clickoXIcon() {
-			var deleteItem =	storageArray.find(function(obj){
-				return obj.content == oTaskText.innerHTML ;				
-			})
-			
-			deleteItem.isDelete = true;	
-
-			oXIcon.parentNode.parentNode.parentNode.removeChild(newTask);				
-			
-			storageArray = storageArray.filter(function(e){
-				return e.isDelete !== true;
-			});
-
-			localStorage.setItem('item', JSON.stringify(storageArray));
-		}
-		oTotalTask.innerHTML = oTaskCount.length + ' ' + 'task left';
-
-	}
-
-	var oTaskCheckboxIcon = document.getElementsByClassName('check-box-icon');
+	// var oTaskCheckboxIcon = document.getElementsByClassName('check-box-icon');
 
 	// oTaskCheckboxIcon.addEventListener('click',test);
 
@@ -218,29 +264,29 @@ window.onload = function () {
 	// 	console.log(1);
 	// }
 
-	oTaskCheckboxIcon.onclick = function(e) {
-		console.log(1);
+	// oTaskCheckboxIcon.onclick = function(e) {
+	// 	console.log(1);
 
-		var oTaskCheckboxIcon1 = document.getElementsByClassName('check-box-icon');
-		labelArray = Array.from(oTaskCheckboxIcon1);
-		var getIndex = labelArray.indexOf(e.target);
-		console.log(getIndex);
-		var arrayCheck = JSON.parse(localStorage.getItem('item'));
-		storageArray[getIndex]['isCheck'] = !storageArray[getIndex]['isCheck'];
-		arrayCheck[getIndex]['isCheck'] = !arrayCheck[getIndex]['isCheck'];		
+	// 	var oTaskCheckboxIcon1 = document.getElementsByClassName('check-box-icon');
+	// 	labelArray = Array.from(oTaskCheckboxIcon1);
+	// 	var getIndex = labelArray.indexOf(e.target);
+	// 	console.log(getIndex);
+	// 	var arrayCheck = JSON.parse(localStorage.getItem('item'));
+	// 	storageArray[getIndex]['isCheck'] = !storageArray[getIndex]['isCheck'];
+	// 	arrayCheck[getIndex]['isCheck'] = !arrayCheck[getIndex]['isCheck'];		
 
-		// oTaskCheckboxIcon.className = item.isCheck ? 'far fa-check-square fa-lg check-box-icon' : 'far fa-square fa-lg check-box-icon'
-		// oTaskCheckboxIcon.style.color = item.isCheck ? '#c8c8c8' : '#000';
-		// oTaskText.style.textDecoration = item.isCheck ? 'line-through' : 'none';
-		// oTaskText.style.color = item.isCheck ? '#c8c8c8' : '#000';
-		// newTask.className = item.isCheck ? 'task task-completed' : 'task in-progress';
+	// 	// oTaskCheckboxIcon.className = item.isCheck ? 'far fa-check-square fa-lg check-box-icon' : 'far fa-square fa-lg check-box-icon'
+	// 	// oTaskCheckboxIcon.style.color = item.isCheck ? '#c8c8c8' : '#000';
+	// 	// oTaskText.style.textDecoration = item.isCheck ? 'line-through' : 'none';
+	// 	// oTaskText.style.color = item.isCheck ? '#c8c8c8' : '#000';
+	// 	// newTask.className = item.isCheck ? 'task task-completed' : 'task in-progress';
 
 
-		stringCheck = JSON.stringify(arrayCheck);
-		localStorage.setItem('item', stringCheck);
-		makeNewTask();
-		console.log(localStorage);
-	}	
+	// 	stringCheck = JSON.stringify(arrayCheck);
+	// 	localStorage.setItem('item', stringCheck);
+	// 	makeNewTask();
+	// 	console.log(localStorage);
+	// }	
 
 	oTotalTask.innerHTML = oTaskCount.length + ' ' + 'task left';
 }
